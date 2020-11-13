03c71e08ece117ba2182a6bd56f6a49f30856ab2
lssnnpscbll <lezzaanne@gmail.com>
2020-11-13 17:28:02 +0800

    Adding index.php to assign_two

--- index.php
+++ index.php
@@ -1,123 +1,30 @@
-<?php 
-//Name: Lessa Anne P. Pascubillo
-//Subject: Advance Programming
-// Professor: Ms. Blances Sanchez ?>
+<?php
+    class User
+    {
+        public $name;
+        public $username;
+        public $email;
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        function add_friend($name)
+        {
+            echo $name . " added a friend</br>";
+        }
+        function post_status($name)
+        {
+            echo $name . " posted a status</br>";
+        }
+    }
 
-        <title>assign_one</title>
-    </head>
-        <body>
-        <div class="container-fluid"><center>
-            <div class="row">
-                <div class="col-md-4">
-                    <br><br><h1><?php echo "Lessa Anne P. Pascubillo" ?></h1>
-                </div>
+    $name = "Lessa Anne Pascubillo";
+    $username = "lssnnpscbll";
+    $email = "lezzaanne@gmail.com";
+    
+    echo "Name: " . $name;
+    echo "Username: " . $username;
+    echo "Email: " . $email . "</br></br>";
 
-                <div class="col-md-4">
-                <form action="" method="post">
-                    <br><br><b>CALCULATOR</b><br><br> 
-                    
-                        Enter First Number:  
-                    <input type="number" name="number1" /><br><br>  
-                        Enter Second Number:  
-                    <input type="number" name="number2" /><br><br>  
+    $user = new User();
+    $user->add_friend($name);
+    $user->post_status($name);
 
-                    <input  type="submit" name="sum" class="button" value="+">
-                    <input  type="submit" name="sub" class="button" value="-"> 
-                    <input  type="submit" name="mul" class="button" value="*"> 
-                    <input  type="submit" name="div" class="button" value="/">   
-                </form> 
-
-                <?php      
-                    if(array_key_exists('sum', $_POST)) { 
-                        sum1(); 
-                    } 
-                    else if(array_key_exists('sub', $_POST)) { 
-                        sub1(); 
-                    } 
-                    else if(array_key_exists('mul', $_POST)) { 
-                        mul1(); 
-                    } 
-                    else if(array_key_exists('div', $_POST)) { 
-                        div1(); 
-                    } 
-                    function sum1() { 
-                        $number1=$_POST['number1'];   
-                        $number2=$_POST['number2'];   
-                        $sum = $number1 + $number2;
-                        echo "<br><br>";
-                        echo "Answer: ".$sum;  
-
-                    } 
-                    function sub1() { 
-                        $number1=$_POST['number1'];   
-                        $number2=$_POST['number2'];   
-                        $sub = $number1 - $number2;
-                        echo "<br><br>";
-                        echo "Answer: ".$sub;   
-                    } 
-                    function mul1() { 
-                        $number1=$_POST['number1'];   
-                        $number2=$_POST['number2'];   
-                        $mul = $number1 * $number2;
-                        echo "<br><br>";
-                        echo "Answer: ".$mul;   
-                    } 
-                    function div1() { 
-                        $number1=$_POST['number1'];   
-                        $number2=$_POST['number2'];   
-                        $div = $number1 / $number2;
-                        echo "<br><br>";
-                        echo "Answer: ".$div;   
-                    } 
-                    ?>
-                </div>
-
-                <div class="col-md-4">
-                <form action="" method="post">
-                        <br><br><b>FINDING GCD</b><br><br>
-                        Enter First Number:  
-                    <input type="number" name="a" /><br><br>  
-                            Enter Second Number:  
-                    <input type="number" name="b" /><br><br>  
-
-                    <input  type="submit" name="submit" value="Submit"> 
-                </form>
-
-                <?php
-
-                    if(array_key_exists('submit', $_POST)) { 
-                        gcd1(); 
-                    } 
-
-                    function gcd1() {
-                        $a=$_POST['a'];   
-                        $b=$_POST['b'];
-                        $gcd; 
-
-                            if ($a > $b)
-                            {
-                            $temp = $a;
-                            $a = $b;
-                            $b = $temp;
-                            }
-                            for($i = 1; $i < ($a+1); $i++)
-                            {
-                            if ($a%$i == 0 and $b%$i == 0)
-                                $gcd = $i;
-                            }
-
-                            echo "<br>GCD: $gcd";
-                    }
-                   
-                    ?>
-                </div>
-            </div>
-        </div></center>
-        </body>
-</html>
+?>
